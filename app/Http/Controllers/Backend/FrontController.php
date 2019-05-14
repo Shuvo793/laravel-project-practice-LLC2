504fa8aef4b49406eac0be1987d79ff6e542f82e
Shuvo793 <cse.engr.shuvo@gmail.com>
2019-05-15 01:24:46 +0600

    file validation and input fields validation concept clear and please study in laravale web for more details

--- app/Http/Controllers/Backend/FrontController.php
+++ app/Http/Controllers/Backend/FrontController.php
@@ -1,9 +1,14 @@
 <?php
 
 namespace App\Http\Controllers\Backend;
+use Illuminate\Support\Str;
 
 use App\Http\Controllers\Controller;
+
 use Illuminate\Http\Request;
+use Validator;
+
+
 
 class FrontController extends Controller
 {
@@ -37,6 +42,10 @@ class FrontController extends Controller
         return view('register',$data);
     }
 
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function processRegistration(Request $request){ //(Request $request) dependence injection sara direct Request() mathod dia kora jae
         //$request->all(); all() dile sob data pabo
 
@@ -55,6 +64,43 @@ class FrontController extends Controller
         /*Request() method getting data*/
         //echo Request()->input('email');
         //echo Request()->input('password');
+
+
+        //validation
+        /*akta form ee akathik input thakte pare akta error dile sob amar notun kore dea lagbe jeta amara nicher ai structure onujaie pabo na*/
+
+        /*$this->validate($request,[
+
+            'email'=>'required|email',
+            'password'=>'required|min:6'
+
+        ]);
+        echo $request->input('email');
+        echo "\n";
+        echo $request->input('password');*/
+
+        /*write way*/
+
+        $validator= Validator::make($request->all(),[ //make() function ta amae amar data gula create kore dae
+
+            'email'=>'required|email',
+            'password'=>'required|min:6',
+            'photo'=>'required|image|max:200'
+
+        ]);
+        $photo=$request->file('photo');
+        $file_name=uniqid('photo_',true). Str::random(40).'.'.$photo->getClientOriginalExtension();
+        if($photo->isValid()){ //isvalid() file ta valid kina ta cheak kore
+            $photo->storeAs('images',$file_name);
+        }
+
+        if($validator->fails()){ //fails() ai function er maddhome jodi operation fail hoe taile kisu akta ghotbe
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        session()->flash('message','Registration successfully');
+
+        return redirect()->back(); //redirect() aita amae sorasori akta page aa nia jaee back() amar ak page pisone nia jacce
 
 
 
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,13 +1,29 @@
 @extends('partsial.master')
 @section('content')
-
-    <form class="form-signin" action="{{ route('register')}}" method="post">
+    @if( session()->has('message'))
+        <div class="alert alert-success">
+            {{ session('message')}}
+        </div>
+    @endif
+    <form class="form-signin" action="{{ route('register')}}" method="post" enctype="multipart/form-data">
         @csrf
         <h1 class="h3 mb-3 font-weight-normal">Please Register</h1>
-        <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" name="email" required autofocus>
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" name="password" required>
+        <label for="inputEmail" >Email address</label>
+        <input type="text" id="inputEmail" class="form-control" placeholder="Email address" name="email" value="{{old('email')}}"  autofocus>
+        @if ($errors->has('email'))
+            <div class="alert-danger">{{ $errors->first('email') }}</div>
+        @endif
+        <label for="inputPassword" >Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" name="password" >
+        @if ($errors->has('password'))
+            <div class="alert-danger">{{ $errors->first('password') }}</div>
+        @endif
+
+        <label for="inputfiles" >Uploads Files</label>
+        <input type="file" id="inputfiles" class="form-control" placeholder="Please upload your file" name="photo" >
+        @if ($errors->has('photo'))
+            <div class="alert-danger">{{ $errors->first('photo') }}</div>
+        @endif
 
         <button class="btn btn-lg btn-primary btn-block" type="submit" name="register">Submit</button>
         <p class="mt-5 mb-3 text-muted">&copy; 2017-2019</p>
